b8cd89297e1ae7883d06871c7797aef7d01bd201
Ayoola <johnsonayoolaojo@gmail.com>
2025-10-15 16:37:33 +0100

    update

---
+++ Layout/Visualizing Designs/JAVASCRIPT/script.js
@@ -0,0 +1,52 @@
+const Heading = document.getElementById('heading').innerHTML ='dATAsLID'
+
+console.log(Heading)
+
+
+console.log("Pastor Sam")
+
+
+let a = 5;
+let b = 10
+let z = a / b
+
+console.log(z)
+
+const  myName = 'Samuel'
+console.log(myName)
+
+
+const fruit = 'mango';
+
+
+const fName = "Ayoola"; 
+ 
+console.log(fName)     // we use 'let' to store a name
+const country = "Nigeria";  // 'const' for fixed value
+let age = 25;               // number value
+
+console.log(name);          // prints "Ayoola"
+console.log(country);       // prints "Nigeria"
+console.log(age);           // prints 25
+
+age = 26;                   // we can change 'let' variable
+console.log(age);   // now prints 26
+
+
+const _fName = 'sgsgsgsgssg'
+
+
+firstName = 'Ayoola '
+middleName = 'John'
+
+fullName = firstName + middleName
+
+console.log(fullName)
+
+
+
+
+
+
+
+
